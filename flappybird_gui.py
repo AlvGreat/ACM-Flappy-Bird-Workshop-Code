205dfb2a2583016a744e8e4cb427a8d97a7c4015
AlvGreat <60948906+AlvGreat@users.noreply.github.com>
2021-04-01 14:14:23 -0700

    Added sound effects

--- flappybird_gui.py
+++ flappybird_gui.py
@@ -1,5 +1,6 @@
 """
 This is the main version with the AI implemented and a GUI with pygame
+Note: sound effects are from Mario, from Gaming Sound FX on youtube, https://www.youtube.com/channel/UCi-xN4ZB6e-0JcXzvBEomlw
 """
 
 import pygame
@@ -8,6 +9,8 @@ import time
 import os 
 import random 
 import pickle # to save our model
+
+pygame.mixer.init(44100, -16, 2, 64) # initialize pygame mixer for game sound effects
 pygame.font.init() # we have to do this for fonts
 
 # size of the window. Note that all caps in python signals a constant
@@ -41,6 +44,16 @@ FLAPPY_BIRD_FONT_SMALL = pygame.font.Font(os.path.join("fonts", "flappybirdy", "
 STAT_FONT_BOLD = pygame.font.Font(os.path.join("fonts", "Roboto", "Roboto-Bold.ttf"), 50)
 STAT_FONT_SMALL = pygame.font.Font(os.path.join("fonts", "Roboto", "Roboto-Regular.ttf"), 25)
 EXTRA_SMALL_FONT = pygame.font.Font(os.path.join("fonts", "Roboto", "Roboto-Regular.ttf"), 15)
+
+# sound effects, use with effect.play()
+JUMP_EFFECT = pygame.mixer.Sound(os.path.join("music", "jump.wav"))
+JUMP_EFFECT.set_volume(0.07)
+
+DEATH_EFFECT = pygame.mixer.Sound(os.path.join("music", "death.wav"))
+DEATH_EFFECT.set_volume(0.2)
+
+POINT_EFFECT = pygame.mixer.Sound(os.path.join("music", "point.wav"))
+POINT_EFFECT.set_volume(0.2)
 
 # ----- we'll create a class for each of the main objects:
 
@@ -255,6 +268,7 @@ class Button:
         # pos is going to be the mouse position returned from pygame.mouse.get_pos()    
         return False
 
+# draws the main game window of the game
 def draw_window(win, birds, pipes, base, score, exit_button=None, gen=None, threshold_msg=None): 
     """
     Draws everything to the window
@@ -292,7 +306,7 @@ def draw_window(win, birds, pipes, base, score, exit_button=None, gen=None, thre
 
     pygame.display.update() # refreshes the window
 
-# main loop of game
+# main loop that trains a new neural network
 def main(genomes, config):
     global gen # declares a global variable 
 
@@ -417,6 +431,7 @@ def main(genomes, config):
             with open("best.pickle", "wb") as f:
                 pickle.dump(nets[0], f)
 
+# main loop with best AI 
 def runBestAI():
     bird = Bird(230, 350) # there will only be one bird
     
@@ -454,6 +469,7 @@ def runBestAI():
         output = model.activate((bird.y, abs(bird.y - pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom)))
         # output is a list of output neurons- we only have one, so use index 0 
         if (output[0] > 0.5): 
+            JUMP_EFFECT.play()
             bird.jump()
 
         rem = [] # list of pipes to remove
@@ -461,6 +477,7 @@ def runBestAI():
         for pipe in pipes: 
             if pipe.collide(bird, win): 
                 run = False 
+                DEATH_EFFECT.play()
                 break
                 
             # if we passed the pipe
@@ -477,6 +494,7 @@ def runBestAI():
         # then we have to add a score and a new pipe
         if add_pipe: 
             score += 1
+            POINT_EFFECT.play()
             pipes.append(Pipe(600)) # add new pipe
 
         #remove everything in the array of pipes to be removed 
@@ -486,11 +504,13 @@ def runBestAI():
         # check collision between bird and floor
         if (bird.y + bird.img.get_height() >= 730 or bird.y < 0):
             run = False
+            DEATH_EFFECT.play()
             break
 
         base.move() # move the base
         draw_window(win, [bird], pipes, base, score, exit_button)
 
+# main loop with manual play
 def runHumanMode():
     bird = Bird(230, 350) # there will only be one bird
 
@@ -519,12 +539,14 @@ def runHumanMode():
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE]:
             bird.jump()
+            JUMP_EFFECT.play()
 
         rem = [] # list of pipes to remove
         add_pipe = False
         for pipe in pipes: 
             if pipe.collide(bird, win): 
                 run = False 
+                DEATH_EFFECT.play()
                 break
                 
             # if we passed the pipe
@@ -541,6 +563,7 @@ def runHumanMode():
         # then we have to add a score and a new pipe
         if add_pipe: 
             score += 1
+            POINT_EFFECT.play()
             pipes.append(Pipe(600)) # add new pipe
 
         #remove everything in the array of pipes to be removed 
@@ -550,11 +573,13 @@ def runHumanMode():
         # check collision between bird and floor
         if (bird.y + bird.img.get_height() >= 730 or bird.y < 0):
             run = False
+            DEATH_EFFECT.play()
             break
 
         base.move() # move the base
         draw_window(win, [bird], pipes, base, score)
 
+# runs the NEAT AI
 def runAI(config_path): 
     # this is a method from the NEAT module to configure what our AI will be like
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
@@ -570,6 +595,7 @@ def runAI(config_path):
     # provide fitness function and how many times this runs
     winner = p.run(main, 50)
 
+# redraws the main gui 
 def draw_main_gui(*argv):
     # fill the window with white and then draw the background
     win.fill((255, 255, 255))
@@ -587,6 +613,7 @@ def draw_main_gui(*argv):
     for x in argv: 
         x.draw(win)
 
+# run the settings gui with the skins
 def run_settings_gui():
     global BIRD_IMGS # we will be updating the bird images
 
@@ -647,7 +674,7 @@ def run_settings_gui():
                     BIRD_IMGS = LILLIAN_BIRD
                     run = False
 
-
+# runs the main gui of the game
 def run_gui(): 
     run = True
     centerX = 250 # center of screen
